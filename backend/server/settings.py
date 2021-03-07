1d8c8bad176efd7ec88be9cf43ef7364457d9b34
morten.aslesen <mortendaehli@gmail.com>
2021-03-07 18:55:58 +0100

    Migrated from sqlite to Postgres

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -121,8 +121,12 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ.get('POSTGRES_HOST', '127.0.0.1'),
+        'NAME': os.environ.get('POSTGRES_DB', 'postgres'),
+        'USER': os.environ.get('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', None),
+        'PORT': os.environ.get('POSTGRES_PORT', 5432)
     }
 }
 
